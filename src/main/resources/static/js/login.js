c6b0ec08aeac7c2bfd45a637266d0b81446ee56d
mshahcode <mikayil.shahtakhtinski@gmail.com>
2024-12-18 10:12:02 -0600

    change jquery logout message to javascript

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,10 +1,12 @@
-$(document).ready(function () {
+document.addEventListener('DOMContentLoaded', function () {
     const urlParams = new URLSearchParams(window.location.search);
     if (urlParams.has('logout') && urlParams.get('logout') === 'true') {
-        const $logoutRow = $('#logout-row');
-        $logoutRow.show();
-        setTimeout(() => {
-            $logoutRow.hide();
-        }, 5_000);
+        const logoutRow = document.getElementById('logout-row');
+        if (logoutRow) {
+            logoutRow.style.display = 'block';
+            setTimeout(() => {
+                logoutRow.style.display = 'none';
+            }, 5000);
+        }
     }
 });
